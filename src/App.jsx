841f61accb8234ddec67b57f85854985159fddd0
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-11-08 18:48:57 +0700

    scrollPhoto:user can scroll infinite photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,61 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const accessKey = import.meta.env.VITE_ACCESS_KEY;
+
+  const fetchPhotos = async () => {
+    setIsLoading(true);
+    try {
+      const url = `https://api.unsplash.com/photos/?client_id=${accessKey}&page=${page}`;
+      const result = await axios.get(url);
+      setPhotos([...photos, ...result.data]);
+      // setPhotos((oldPhoto) => {
+      //   return [...oldPhoto, ...result.data];
+      // });
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, [page]);
+
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      if (
+        window.innerHeight + window.scrollY >
+          document.body.offsetHeight - 500 &&
+        !isLoading
+      ) {
+        setPage((oldPage) => oldPage + 1);
+      }
+    });
+    return () => window.removeEventListener("scroll", event);
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center tracking-wider text-5xl font-bold m-5">
+      <h1>Infinite Scroll Photo | Unsplash API</h1>
+      {photos.map((photo, index) => {
+        return (
+          <img
+            src={photo.links.download}
+            alt={photo.alt_description}
+            key={index}
+            className="w-4/6 m-4"
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
